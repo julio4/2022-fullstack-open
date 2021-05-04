ab35a7d8fd8cddbbf4994dc3268ff39eb5ab5c8a
julio4 <jules.doumeche@gmail.com>
2021-05-04 21:24:05 +0200

    ✨ part 2 b / c

--- part 2/README.md
+++ part 2/README.md
@@ -3,26 +3,31 @@
 ## [Part 2 - Communicating with server](https://fullstackopen.com/en/part2)
 
 ### a. Rendering a collection, modules
+
 [Course materials](https://fullstackopen.com/en/part2/rendering_a_collection_modules)
 
 - Exercises 2.1 -> 2.5 [see code](./a%20Rendering%20a%20collection)
 
 ### b. Forms
+
 [Course materials](https://fullstackopen.com/en/part2/forms)
 
 - Exercises 2.6 -> 2.10 [see code](./b%20Forms)
 
 ### c. Getting data from server
+
 [Course materials](https://fullstackopen.com/en/part2/getting_data_from_server)
 
-- Exercises 2.11 -> 2.14
+- Exercises 2.11 -> 2.14 [see code](./c%20Getting%20data%20rom%20server)
 
 ### d. Altering data in server
+
 [Course materials](https://fullstackopen.com/en/part2/altering_data_in_server)
 
 - Exercises 2.15 -> 2.18
 
 ### e. Adding styles to React app
+
 [Course materials](https://fullstackopen.com/en/part2/adding_styles_to_react_app)
 
 - Exercises 2.19 -> 2.20
---
+++ part 2/b Forms/app_2.10/App.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+
+import Persons from "./components/Persons";
+import PersonForm from "./components/PersonForm";
+import Filter from "./components/Filter";
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
+
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [searchName, setSearchName] = useState("");
+
+  const addName = (event) => {
+    event.preventDefault();
+    if (!persons.some((person) => person.name === newName)) {
+      const nameObject = {
+        name: newName,
+        number: newNumber,
+      };
+      setPersons(persons.concat(nameObject));
+    } else {
+      alert(`${newName} is already added to phonebook`);
+    }
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleSearchName = (event) => {
+    setSearchName(event.target.value);
+  };
+
+  const personsToShow = persons.filter((person) =>
+    person.name.toLowerCase().includes(searchName.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <Filter value={searchName} handler={handleSearchName} />
+
+      <h2>Add a new</h2>
+
+      <PersonForm
+        submit={addName}
+        name={newName}
+        nameHandler={handleNameChange}
+        number={newNumber}
+        numberHandler={handleNumberChange}
+      />
+
+      <h2>Numbers</h2>
+
+      <Persons list={personsToShow} />
+    </div>
+  );
+};
+
+export default App;
