f6e41f6b4c182af38759ece2c87a63518b7af95d
Julio4 <jules.doumeche@gmail.com>
2022-08-05 17:10:10 +0200

    deleted

--- part 2/e Adding styles to react app/2.20/components/Notification.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Notification = ({ state, message }) => {
-  return message !== null ? (
-    <div className={`notif ${state}`}>{message}</div>
-  ) : null;
-};
-
-export default Notification;
