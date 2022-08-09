a7b6c0951e7ca6aa0daaa6c7dd27e616b0e76cd1
Julio4 <jules.doumeche@gmail.com>
2022-08-09 20:51:23 +0200

    added: ultimate-hooks

--- src/App.js
+++ src/App.js
@@ -1,74 +1,72 @@
-import {
-  Routes, Route, useMatch
-} from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-import { useState } from 'react'
+const useField = (type) => {
+  const [value, setValue] = useState('')
 
-import Menu from './components/Menu'
-import AnecdoteList from './components/AnecdoteList'
-import Anecdote from './components/Anecdote'
-import About from './components/About'
-import Footer from './components/Footer'
-import AnecdoteForm from './components/AnecdoteForm'
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+const useResource = (baseUrl) => {
+  const [resources, setResources] = useState([])
+
+  // ...
+
+  const create = (resource) => {
+    // ...
+  }
+
+  const service = {
+    create
+  }
+
+  return [
+    resources, service
+  ]
+}
 
 const App = () => {
-  const [anecdotes, setAnecdotes] = useState([
-    {
-      content: 'If it hurts, do it more often',
-      author: 'Jez Humble',
-      info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
-      votes: 0,
-      id: 1
-    },
-    {
-      content: 'Premature optimization is the root of all evil',
-      author: 'Donald Knuth',
-      info: 'http://wiki.c2.com/?PrematureOptimization',
-      votes: 0,
-      id: 2
-    }
-  ])
+  const content = useField('text')
+  const name = useField('text')
+  const number = useField('text')
 
-  const [notification, setNotification] = useState('')
+  const [notes, noteService] = useResource('http://localhost:3005/notes')
+  const [persons, personService] = useResource('http://localhost:3005/persons')
 
-  const addNew = (anecdote) => {
-    anecdote.id = Math.round(Math.random() * 10000)
-    setAnecdotes(anecdotes.concat(anecdote))
+  const handleNoteSubmit = (event) => {
+    event.preventDefault()
+    noteService.create({ content: content.value })
   }
-
-  const anecdoteById = (id) =>
-    anecdotes.find(a => a.id === id)
-
-  const vote = (id) => {
-    const anecdote = anecdoteById(id)
-
-    const voted = {
-      ...anecdote,
-      votes: anecdote.votes + 1
-    }
-
-    setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
+ 
+  const handlePersonSubmit = (event) => {
+    event.preventDefault()
+    personService.create({ name: name.value, number: number.value})
   }
-
-  const match = useMatch('/anecdotes/:id')
-  const anecdote = match
-    ? anecdotes.find(a => a.id === Number(match.params.id))
-    : null
 
   return (
     <div>
-      <h1>Software anecdotes</h1>
-        <Menu />
-        {notification}
+      <h2>notes</h2>
+      <form onSubmit={handleNoteSubmit}>
+        <input {...content} />
+        <button>create</button>
+      </form>
+      {notes.map(n => <p key={n.id}>{n.content}</p>)}
 
-        <Routes>
-          <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
-          <Route path='/anecdotes/:id' element={<Anecdote anecdote={anecdote} />} />
-          <Route path="/about" element={< About />} /> 
-          <Route path="/create" element={<AnecdoteForm addNew={addNew} notification={setNotification}/>} /> 
-        </Routes> 
-
-      <Footer />
+      <h2>persons</h2>
+      <form onSubmit={handlePersonSubmit}>
+        name <input {...name} /> <br/>
+        number <input {...number} />
+        <button>create</button>
+      </form>
+      {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
     </div>
   )
 }
