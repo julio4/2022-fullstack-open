b7cf50bf7e282cece93b8415bb167142acfb2525
julio4 <jules.doumeche@gmail.com>
2022-08-13 22:25:08 +0200

    added: blog-backend

---
+++ tests/list.test.js
@@ -0,0 +1,55 @@
+const listHelper = require('../utils/list_helper')
+const helper = require('./test_helper')
+
+const blogs = helper.initialBlogs
+
+test('dummy returns one', () => {
+  const result = listHelper.dummy([])
+  expect(result).toBe(1)
+})
+
+describe('total likes', () => {
+  const listWithOneBlog = [blogs[0]]
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(7)
+  })
+
+  test('Total likes of multiples blogs posts', () => {
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(36)
+  })
+})
+
+describe('favorite like', () => {
+  test('Favorite blog post of a list of blogs', () => {
+    const result = listHelper.favoriteBlog(blogs)
+    expect(result).toEqual(blogs[2])
+  })
+
+  test('when list has only one blog, equal to this blog post', () => {
+    const result = listHelper.favoriteBlog([blogs[0]])
+    expect(result).toEqual(blogs[0])
+  })
+})
+
+describe('most blogs', () => {
+  test('Author with most blog post in a list', () => {
+    const result = listHelper.mostBlogs(blogs)
+    expect(result).toEqual({
+      author: 'Robert C. Martin',
+      blogs: 3
+    })
+  })
+})
+
+describe('most likes', () => {
+  test('Author with most likes in a list', () => {
+    const result = listHelper.mostLikes(blogs)
+    expect(result).toEqual({
+      author: 'Edsger W. Dijkstra',
+      likes: 17
+    })
+  })
+})
